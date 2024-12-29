805792ccfcb652e54f01a9ab126ed828ce55a219
Adejokun Ibukunoluwa <Adejokunibk@gmail.com>
2024-12-29 16:07:19 +0100

    interface created and updated

---
+++ src/interface/admin-data.ts
@@ -0,0 +1,10 @@
+export interface AdminData {
+  username: string;
+  emailAddy: string;
+  firstname: string;
+  middlename: string;
+  lastname: string;
+  dateCreated: string;
+  id: number;
+  is_Active: boolean;
+}
