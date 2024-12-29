80b814c6aa4999655adac0508410294aae4f63c2
Adejokun Ibukunoluwa <Adejokunibk@gmail.com>
2024-12-29 16:06:38 +0100

    routes updated, signin set as index

--- src/routes/sections.tsx
+++ src/routes/sections.tsx
@@ -14,6 +14,8 @@ export const HomePage = lazy(() => import('src/pages/home'));
 export const BlogPage = lazy(() => import('src/pages/blog'));
 export const UserPage = lazy(() => import('src/pages/user'));
 export const SignInPage = lazy(() => import('src/pages/sign-in'));
+export const SignUpPage = lazy(() => import('src/pages/sign-up'));
+export const ForgotPasswordPage = lazy(() => import('src/pages/forgot-password')); 
 export const ProductsPage = lazy(() => import('src/pages/products'));
 export const Page404 = lazy(() => import('src/pages/page-not-found'));
 
@@ -34,7 +36,11 @@ const renderFallback = (
 
 export function Router() {
   return useRoutes([
-    {
+  {
+    path: '/',
+    element: <Navigate to = 'sign-in' replace />
+  }
+    ,{
       element: (
         <DashboardLayout>
           <Suspense fallback={renderFallback}>
@@ -43,7 +49,7 @@ export function Router() {
         </DashboardLayout>
       ),
       children: [
-        { element: <HomePage />, index: true },
+        { path: 'dashboard', element: <HomePage /> },
         { path: 'user', element: <UserPage /> },
         { path: 'products', element: <ProductsPage /> },
         { path: 'blog', element: <BlogPage /> },
@@ -54,8 +60,24 @@ export function Router() {
       element: (
         <AuthLayout>
           <SignInPage />
-        </AuthLayout>
+         </AuthLayout>
       ),
+    },
+    {
+      path: 'sign-up',
+      element: (
+        <AuthLayout>
+        <SignUpPage />
+        </AuthLayout>
+      )
+    },
+    {
+      path: 'forgot-password',
+      element: (
+        <AuthLayout>
+          <ForgotPasswordPage/>
+        </AuthLayout>
+      )
     },
     {
       path: '404',
