b860e67e7f1dc7404624c1c85ff0698214d0bfdf
Adejokun Ibukunoluwa <Adejokunibk@gmail.com>
2025-01-21 16:15:41 +0100

    changes made to routing

--- src/routes/sections.tsx
+++ src/routes/sections.tsx
@@ -15,6 +15,7 @@ export const BlogPage = lazy(() => import('src/pages/blog'));
 export const UserPage = lazy(() => import('src/pages/user'));
 export const SignInPage = lazy(() => import('src/pages/sign-in'));
 export const SignUpPage = lazy(() => import('src/pages/sign-up'));
+export const VerifyPage = lazy(() => import('src/pages/verify'))
 export const ForgotPasswordPage = lazy(() => import('src/pages/forgot-password')); 
 export const ProductsPage = lazy(() => import('src/pages/products'));
 export const Page404 = lazy(() => import('src/pages/page-not-found'));
@@ -68,6 +69,14 @@ export function Router() {
       element: (
         <AuthLayout>
         <SignUpPage />
+        </AuthLayout>
+      )
+    },
+    {
+      path: 'verify/:ecode',
+      element: (
+        <AuthLayout>
+          <VerifyPage/>
         </AuthLayout>
       )
     },
